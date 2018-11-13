632b7e44d7b2a460e3e75379e64833a245ea0d35
Andrew <andr3wcarpentier@gmail.com>
2018-11-13 13:18:29 +0100

    ajout du css

--- src/main/java/fr/andrew/pizzeria/pizzeria/controller/impl/PizzaControllerImpl.java
+++ src/main/java/fr/andrew/pizzeria/pizzeria/controller/impl/PizzaControllerImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -26,7 +27,7 @@ public class PizzaControllerImpl implements IPizzaController{
         return "pizza/pizza";
     }
 
-    @GetMapping("/detail")
+    @GetMapping("/pizza/detail")
     public String findOneById(Model model, @RequestParam Integer id) {
         Pizza pizza = pizzaBusiness.findOneById(id);
         model.addAttribute("pizza", pizza);
